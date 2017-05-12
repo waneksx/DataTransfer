f642e962f780abff43323bc330fd964abba9d877
Alexey Litvin <alexey.litvin@gilliesoftware.com>
2017-05-12 16:28:10 +0300

    Added Call entitie

---
+++ Transfer/Entities/Call.cs
@@ -0,0 +1,29 @@
+﻿using MessagePack;
+
+namespace Entities
+{
+    [MessagePackObject]
+    public class Call
+    {
+        [Key(0)]
+        public long Id { get; set; }
+        [Key(1)]
+        public string CallExternalId { get; set; }
+        [Key(2)]
+        public long CampaignId { get; set; }
+        [Key(3)]
+        public long AgentId { get; set; }
+        [Key(4)]
+        public string PhoneNumber { get; set; }
+        [Key(5)]
+        public string DialedPhoneNumber { get; set; }
+        [Key(6)]
+        public System.DateTime StartTime { get; set; }
+        [Key(7)]
+        public System.DateTime EndTime { get; set; }
+        [Key(8)]
+        public int Status { get; set; }
+        [Key(9)]
+        public bool WasSentToHub { get; set; }
+    }
+}
