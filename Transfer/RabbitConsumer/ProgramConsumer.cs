181d6a522604ea629b9959e43071ff8c4509575a
Ivan Sakharchuk <ivan.sakharchuk@gilliesoftware.com>
2017-05-18 16:03:57 +0300

    using amqp cloud

--- Transfer/RabbitConsumer/ProgramConsumer.cs
+++ Transfer/RabbitConsumer/ProgramConsumer.cs
@@ -8,9 +8,17 @@ namespace RabbitConsumer
 {
     class ProgramConsumer
     {
+        static long transered = 0;
+        static DateTime time = DateTime.Now;
+
         static void Main(string[] args)
         {
-            var factory = new ConnectionFactory() { UserName = "ivan", Password = "ivan", VirtualHost = "fr", HostName = "10.158.5.145" };
+            var factory = new ConnectionFactory() {
+                Uri = "amqp://eeoeckin:H0sB6bMrzzXlGNyENalmX1C38WKgHNpK@lark.rmq.cloudamqp.com/eeoeckin",
+                VirtualHost = "eeoeckin",
+                UserName = "eeoeckin",
+                Password = "H0sB6bMrzzXlGNyENalmX1C38WKgHNpK"
+            };
             using (var connection = factory.CreateConnection())
             using (var channel = connection.CreateModel())
             {
@@ -26,7 +34,9 @@ namespace RabbitConsumer
                     var body = ea.Body;
                     var message = Encoding.UTF8.GetString(body);
                     var calls = CallJson.JsonToCalls(message);
-                    Console.WriteLine(" [x] Received JSON string with length {0}, call array length {1}", message.Length, calls.Length);
+                    transered += calls.Length;
+                    Console.Beep(1400, 100);
+                    Console.WriteLine($" [x] Recived JSON string with length {message.Length}, calls length {calls.Length}, totaly tarnsfered {transered} calls per {(DateTime.Now - time).ToString(@"h\:mm\:ss")} ");
                 };
                 channel.BasicConsume(queue: "call",
                                      noAck: true,
--- Transfer/RabbitProducer/ProgramProducer.cs
+++ Transfer/RabbitProducer/ProgramProducer.cs
@@ -8,9 +8,17 @@ namespace RabbitProducer
     class ProgramProducer
     {
         static int cycle = 0;
+        static long transered = 0;
+        static DateTime time = DateTime.Now;
         static void Main(string[] args)
         {
-            var factory = new ConnectionFactory() { HostName = "localhost", VirtualHost = "fr", UserName = "alex", Password = "alex" };
+            var factory = new ConnectionFactory()
+            {
+                Uri = "amqp://eeoeckin:H0sB6bMrzzXlGNyENalmX1C38WKgHNpK@lark.rmq.cloudamqp.com/eeoeckin",
+                VirtualHost = "eeoeckin",
+                UserName = "eeoeckin",
+                Password = "H0sB6bMrzzXlGNyENalmX1C38WKgHNpK"
+            };
             using (var connection = factory.CreateConnection("alex"))
             using (var channel = connection.CreateModel())
             {
@@ -21,16 +29,16 @@ namespace RabbitProducer
                                      arguments: null);
                 while (true)
                 {
-                    cycle++;
                     var calls = CallJson.GetCalls();
                     string message = CallJson.CallsToJson(calls);
+                    transered += calls.Length;
                     var body = Encoding.UTF8.GetBytes(message);
 
                     channel.BasicPublish(exchange: "",
                                          routingKey: "call",
                                          basicProperties: null,
                                          body: body);
-                    Console.WriteLine(" [x] Sent JSON string with length {0}, calls length {1}", message, calls.Length);
+                    Console.WriteLine($" [x] Sent JSON string with length {message.Length}, calls length {calls.Length}, totaly tarnsfered {transered} calls per {(DateTime.Now - time).ToString(@"h\:mm\:ss")} ");
                 }
             }
 
